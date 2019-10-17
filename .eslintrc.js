ce3ea91357b4d746dba458cd888e92f7903267fd
jiyuujin LAB <jiyuujinunite@gmail.com>
2019-10-17 20:03:03 +0900

    no-hoge を削除

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,6 @@ module.exports = {
         parser: 'babel-eslint'
     },
     rules: {
-        'no-hoge': 2,
         indent: ['error', 4],
         quotes: [2, 'single'],
         'linebreak-style': [2, 'unix'],
--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 module.exports = {
     rules: {
-        'no-hoge': 2,
         indent: ['error', 4],
         quotes: [2, 'single'],
         'linebreak-style': [2, 'unix'],
