1ede632751a18ba0d9432c9fb8639ee8a0e7f724
jiyuujin LAB <jiyuujinunite@gmail.com>
2020-05-04 10:41:53 +0900

    Improve local development

---
+++ packages/vue/hoge.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    {{ filter }}
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default {
+  data() {
+    return {
+      abc: '123'
+    }
+  },
+  computed: {
+    filter() {
+      return 'Go: ' + this.abc
+    }
+  }
+}
+</script>
